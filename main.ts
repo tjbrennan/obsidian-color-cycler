fb7462c3d713a0ba2fb353627f9af2085806fbf7
Taylor Brennan <taylor.j.brennan@gmail.com>
2023-02-18 18:38:27 +0800

    implement basic color cycling with button, command, settings

--- main.ts
+++ main.ts
@@ -1,137 +1,104 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ColorCyclerSettings {
+	increment: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ColorCyclerSettings = {
+	increment: "30",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ColorCycler extends Plugin {
+	settings: ColorCyclerSettings;
+	hue: string;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.hue = document.body.style.getPropertyValue("--accent-h");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const ribbonIconEl = this.addRibbonIcon(
+			"palette",
+			"Sample Plugin",
+			(evt: MouseEvent) => {
+				this.cycleColor();
+			}
+		);
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "cycle-color",
+			name: "Cycle accent color",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				this.cycleColor();
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText(`Hue ${this.hue}`);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		ribbonIconEl.addEventListener("click", () =>
+			statusBarItemEl.setText(`Hue ${this.hue}`)
+		);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ColorCyclerSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+	cycleColor() {
+		let hue = parseInt(this.hue) || 0;
+		const increment = parseInt(this.settings.increment) || 30;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		if (hue % increment !== 0 || hue % 360 === 0) {
+			hue = 0;
+		}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		this.hue = (hue + increment).toString();
+
+		document.body.style.setProperty("--accent-h", this.hue);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ColorCyclerSettingTab extends PluginSettingTab {
+	plugin: ColorCycler;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ColorCycler) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for Color Cycler" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Color wheel increment")
+			.setDesc("How many degrees to advance the color wheel")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter degrees")
+					.setValue(this.plugin.settings.increment)
+					.onChange(async (value) => {
+						this.plugin.settings.increment = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
